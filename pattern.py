4f5c0b8658027ac7e4a660bf42cdf5c557f2288e
Yakavi <80531723+YAKAVI@users.noreply.github.com>
2021-03-12 21:48:44 +0530

    Update and rename pattern.txt to pattern.py

---
+++ pattern.py
@@ -0,0 +1,15 @@
+for i in range(1,6):
+    for j in range(5,i,-1):
+            print('',end='')
+        for k in range(0,i)
+            print('*',end='')
+        print()
+
+n=int(input("Enter the number of rows:")
+k=order("A")
+for i in range(n):
+    for j in range(j+1):
+            print(chr=(k),end='')
+            k=k+1
+        print()
+
