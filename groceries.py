6f64e8bd590fbae8696b435ef237312db93f7d6c
Yakavi <80531723+YAKAVI@users.noreply.github.com>
2021-03-12 20:40:30 +0530

    Update and rename README.md to groceries.py

--- groceries.py
+++ groceries.py
@@ -1,8 +1,3 @@
-### Hi there 👋
-
-<!--
-**YAKAVI/Yakavi** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-
 # This loop will go on until the budget is integer or float 
 while True: 
 	try: 
